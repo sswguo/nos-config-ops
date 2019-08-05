9d5f626b446de3c34ad3489fafd73b9fd9b88a7b
Wenjie Guo <wguo@redhat.com>
2019-08-05 14:03:14 +0800

    update script

--- build_ansible_config.py
+++ build_ansible_config.py
@@ -2,10 +2,33 @@
 
 import os
 import subprocess
+import sys
+import shutil
+import pipes
 
-# subprocess.call('ansible-vault encrypt_string --vault-password-file /Users/wguo/.nos/ansible_vault_password "12345"', shell=True)
+# pro_dir = "/Users/wguo/GitRepo/pnc-indy-etc/indy/upshift/newcastle-devel/indy-master/"
+
+try:
+  pro_dir = sys.argv[1]
+except IndexError as e:
+  print "Please specify the target etc dir."
+  sys.exit(e)
+
+# Default true
+Dryrun = 1
+
+try:
+  Dryrun = sys.argv[2]
+except IndexError:
+  print "Use default setting: Dryrun is enabled."
+
+print "Dryrun: " + Dryrun
 
 # ~/GitRepo/nos-ansible-playbooks/playbooks/newcastle/vaults/indy-master/
+
+vault_password_file = "~/.nos/ansible_vault_password"
+
+work_dir = "/tmp/wd/"
 
 # Encrypt the credentials via ansible vault
 
@@ -23,22 +46,60 @@ list_dir = [ "etc/brew/", "etc/keycloak/"  ]
 # - kafka secrets
 # - proxy secrets
 
-list_dir_2 = ["etc/kafka/", "etc/proxy/"]
+list_dir_2 = ["etc/proxy/"]
 
+def run(cmd):
+  print cmd
+  if Dryrun == "0":
+    print ">>"
+    subprocess.call(cmd, shell=True)
+    print "<<"
 
+# Encrypt the credentials via ansible vault 
+# 
+# ansible-vault encrypt --vault-password-file ~/.nos/ansible_vault_password <files>
+#
 def vault():
   print "##### Ansible Vault ######"
   for d in list_dir:
     print d
-    # TODO    
+    print "-------------"
+    for file in os.listdir(work_dir + d):
+      print file
+      cmd = 'ansible-vault encrypt --vault-password-file {} {}'.format(vault_password_file, pipes.quote(work_dir + d + file))
+      run(cmd)
+      print ""
+  print ""    
 
+
+# for several special files like keystore/truststores, need to encoded by base64 first
+#
+# base64 data.txt > data.b64
+#
 def b64_and_vault():
   print "##### Base64 and Ansible Vault ######"
   for d in list_dir_2:
     print d
-    # TODO
+    print "-------------"
+    for file in os.listdir(work_dir + d):
+      print file
+      cmd = 'base64 {} > {}'.format(pipes.quote(work_dir + d + file), pipes.quote(work_dir + d + file+'.b64'))
+      run(cmd) 
+      cmd = 'ansible-vault encrypt --vault-password-file {} {}'.format(vault_password_file, pipes.quote(work_dir + d + file+'.b64'))
+      run(cmd)
+      print ""
+  print ""
+
+def prepare():
+  shutil.rmtree(work_dir)
+  shutil.copytree(pro_dir, work_dir)
+  for file in os.listdir(work_dir):
+    print file
+
+  print ""
 
 def main():
+  prepare()
   vault()
   b64_and_vault()
   
